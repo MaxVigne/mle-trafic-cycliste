9308b29bb6df9bb16299c04669ab1076741b23cd
Eliah Rebstock <rebeliah@protonmail.com>
2025-05-14 16:24:48 +0200

    Create train_model.py

---
+++ src/models/train_model.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.model_selection import train_test_split
+from pathlib import Path
+import joblib
+
+reg_df = pd.read_csv('data/processed/lieu-compteur-one-hot-encoded.csv', index_col=0)
+X = reg_df.drop(columns=["Comptage horaire"])
+col_norm = ["Jour", "Mois", "Année", "Heure", "Jour_semaine", "Jour férié", "Vacances scolaires"]
+
+# Encodage des features
+encoder = OneHotEncoder(sparse_output=False, dtype=int)
+array = encoder.fit_transform(X[col_norm])
+encoded_df_clean = pd.DataFrame(array, columns=encoder.get_feature_names_out(col_norm))
+encoded_df_clean.index = X.index
+X_clean = pd.concat([X.drop(columns=col_norm), encoded_df_clean], axis=1)
+
+# Transformation logarithmique de la variable cible pour éviter les valeurs aberrantes
+y = np.log1p(reg_df["Comptage horaire"])  # log(1+x) pour gérer les zéros
+X_train, X_test, y_train, y_test = train_test_split(X_clean, y, test_size=0.2, random_state=42)
+
+learning_rate = 0.5
+max_iter = 500
+params = {'learning_rate': learning_rate, 'max_iter': max_iter}
+MODELS_DIR = Path("models")
+
+model_filename = MODELS_DIR / f"hgb_regressor_{learning_rate}_{max_iter}.pkl"
+model = HistGradientBoostingRegressor(**params)
+model.fit(X_train, y_train)
+joblib.dump(model, model_filename)
