712a890a64e1c57248ad1b3da26a0486879d64d5
Eliah Rebstock <rebeliah@protonmail.com>
2025-05-20 15:56:28 +0200

    Update API tests to use authentication

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,4 +1,5 @@
 import requests
+import pytest
 
 API_URL = "http://localhost:8000"
 
@@ -13,19 +14,37 @@ MODEL_INPUT_PARAMS = {
     "site_comptage": "Pont des Invalides"
 }
 
+VALID_USER = {"username": "johndoe", "password": "secret"}
+
+def get_token(username, password):
+    """Helper function to get a token"""
+    response = requests.post(
+        f"{API_URL}/login",
+        data={"username": username, "password": password}
+    )
+    if response.status_code == 200:
+        return response.json().get("access_token")
+    return None
+
+@pytest.fixture
+def access_token():
+   return get_token(VALID_USER["username"], VALID_USER["password"])
+
 def test_api_root():
     r = requests.get(f"{API_URL}/")
     assert r.status_code == 200
     assert r.json() == {"message": "Welcome to the Traffic Cycliste Prediction API"}
 
-def test_predict():
-    r = requests.post(f"{API_URL}/predict", json=MODEL_INPUT_PARAMS)
+def test_predict(access_token):
+    headers = {"Authorization": f"Bearer {access_token}"}
+    r = requests.post(f"{API_URL}/predict", json=MODEL_INPUT_PARAMS, headers=headers)
     assert r.status_code == 200
     assert r.json()["input_parameters"] == MODEL_INPUT_PARAMS
     assert r.json()["prediction"] # assert that the prediction value exists
 
-def test_predict_missing_data():
-    r = requests.post(f"{API_URL}/predict", json={})
+def test_predict_missing_data(access_token):
+    headers = {"Authorization": f"Bearer {access_token}"}
+    r = requests.post(f"{API_URL}/predict", json={}, headers=headers)
     assert r.status_code == 422
 
 def test_metrics():
